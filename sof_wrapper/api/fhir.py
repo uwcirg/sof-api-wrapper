a809d867c5f68ab98cac4f29416ee4ee01dd5bc7
Ivan Cvitkovic <ivanc@uw.edu>
2020-02-28 15:46:42 -0800

    wip

--- sof_wrapper/api/fhir.py
+++ sof_wrapper/api/fhir.py
@@ -14,6 +14,18 @@ def medication_order():
     response.raise_for_status()
     return response.json()
 
+
+# /Observation?patient._id=53b07006-f454-ea11-8241-0a0332b55c97
+@blueprint.route('/Observation')
+def medication_order():
+    pdmp_url = '{base_url}/v/r2/fhir/MedicationOrder'.format(
+        base_url=current_app.config['PHR_URL'],
+    )
+    response = requests.get(pdmp_url)
+    response.raise_for_status()
+    return response.json()
+
+
 @blueprint.after_request
 def add_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
