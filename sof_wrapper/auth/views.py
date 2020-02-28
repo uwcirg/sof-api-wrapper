970ec31c31fbbe41236fa07f82cc9d15b3dcaebb
Ivan Cvitkovic <ivanc@uw.edu>
2020-02-28 13:18:39 -0800

    Raise exception on non-OK response

--- sof_wrapper/auth/views.py
+++ sof_wrapper/auth/views.py
@@ -29,7 +29,11 @@ def launch():
 
     # fetch conformance statement from /metadata
     ehr_metadata_url = '%s/metadata' % iss
-    metadata = requests.get(ehr_metadata_url, headers={'Accept': 'application/json'})
+    metadata = requests.get(
+        ehr_metadata_url,
+        headers={'Accept': 'application/json'},
+    )
+    metadata.raise_for_status()
     metadata_security = metadata.json()['rest'][0]['security']
 
     # todo: use less fragile lookup logic (JSONPath?)
