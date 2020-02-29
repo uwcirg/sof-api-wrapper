826b58a057f0f72fdc3780bd7ec7e2a3b5006317
Ivan Cvitkovic <ivanc@uw.edu>
2020-02-28 17:19:33 -0800

    Add passthrough for ODH query

--- sof_wrapper/api/fhir.py
+++ sof_wrapper/api/fhir.py
@@ -17,13 +17,23 @@ def medication_order():
 
 # /Observation?patient._id=53b07006-f454-ea11-8241-0a0332b55c97
 @blueprint.route('/Observation')
-def medication_order():
-    pdmp_url = '{base_url}/v/r2/fhir/MedicationOrder'.format(
+def observations():
+    phr_url = '{base_url}/Observation'.format(
         base_url=current_app.config['PHR_URL'],
     )
-    response = requests.get(pdmp_url)
-    response.raise_for_status()
-    return response.json()
+    # todo: lookup from frontend with Patient details
+    phr_params = {'patient._id': '53b07006-f454-ea11-8241-0a0332b55c97'}
+
+    phr_observations = requests.get(
+        url=phr_url,
+        params=phr_params,
+        headers={
+            'Authorization': 'Bearer %s' % current_app.config['PHR_TOKEN'],
+            'Accept': 'application/json',
+        },
+    )
+    phr_observations.raise_for_status()
+    return phr_observations.json()
 
 
 @blueprint.after_request
