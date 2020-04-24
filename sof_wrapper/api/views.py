9930e5c880b0c7df38c93cacb964507293af77e0
Paul Bugni <pbugni@u.washington.edu>
2020-04-23 17:46:06 -0700

    Initial take at logging additional parameters in JSON format

--- sof_wrapper/api/views.py
+++ sof_wrapper/api/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, current_app
 
 
 base_blueprint = Blueprint('base', __name__)
@@ -6,6 +6,8 @@ base_blueprint = Blueprint('base', __name__)
 
 @base_blueprint.route('/')
 def root():
+    current_app.logger.warn(
+        'example message', extra={'extra_i': 12, 'extra_j': 'whee'})
     return {'ok':True}
 
 @base_blueprint.after_request
