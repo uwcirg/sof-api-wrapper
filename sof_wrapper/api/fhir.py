d080c5115730cfd382725f62508be5bc71c62aca
Ivan Cvitkovic <ivanc@uw.edu>
2020-07-03 16:23:06 -0700

    Move entire PDMP URL to config; remove path params

--- sof_wrapper/api/fhir.py
+++ sof_wrapper/api/fhir.py
@@ -7,9 +7,7 @@ blueprint = Blueprint('fhir', __name__, url_prefix='/v/r2/fhir/')
 
 @blueprint.route('/MedicationOrder')
 def medication_order():
-    pdmp_url = '{base_url}/v/r2/fhir/MedicationOrder'.format(
-        base_url=current_app.config['PDMP_URL'],
-    )
+    pdmp_url = current_app.config['PDMP_URL']
     response = requests.get(pdmp_url)
     response.raise_for_status()
     return response.json()
