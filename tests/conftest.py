aca610284ecd09d547ddab04d63d64711c712b2b
Paul Bugni <pbugni@u.washington.edu>
2020-08-19 22:46:59 -0700

    add client fixture for common need

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,9 @@ from pytest import fixture
 def app():
     from sof_wrapper.app import create_app
     return create_app(testing=True)
+
+
+@fixture
+def client(app):
+    with app.test_client() as c:
+        yield c
