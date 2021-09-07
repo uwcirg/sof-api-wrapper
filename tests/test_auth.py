a2a3f91421b4da708c1589e2ae54611b66bf8025
Paul Bugni <pbugni@u.washington.edu>
2021-09-07 16:38:19 -0700

    simple test to confirm DEA extraction from JWT works as expected (using a scraped example)

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -24,6 +24,11 @@ def example_token():
         'qQ'))
 
 
+@fixture
+def jwt_sample():
+    return "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJfcW5PSE1xeDhidGpldW1CQUVvNjBaQVJoWThxd2Y0WU1ncU1GOEZreHNFIn0.eyJqdGkiOiI4ZGZmMmNiNy1hODUzLTQ4ZWEtOGZjMS01M2ZkMzVjZDEwMGQiLCJleHAiOjE2MzA3MTYwMDksIm5iZiI6MCwiaWF0IjoxNjMwNzE0MjA5LCJpc3MiOiJodHRwczovL2tleWNsb2FrLnBiLmRldi5jb3NyaS5jaXJnLndhc2hpbmd0b24uZWR1L2F1dGgvcmVhbG1zL2ZFTVIiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiNzk0ZDI3ODItZTc4My00Y2JiLWFlNzItYjE2NWE0YzA2ZTdmIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiY29zcmlfb3BlbmlkX2NsaWVudCIsImF1dGhfdGltZSI6MTYzMDcxNDE2Miwic2Vzc2lvbl9zdGF0ZSI6ImI5NzRhZDE1LTg4ZWYtNDFhNi04OGIzLTE1Njk0N2JjZjViOCIsImFjciI6IjAiLCJhbGxvd2VkLW9yaWdpbnMiOlsiKiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJERUEiOiJ0ZXN0REVBdmFsdWUiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInByZWZlcnJlZF91c2VybmFtZSI6InRlc3QifQ.A5LFRH-obxsGov1RMorpY0UFQHiMjL5nbl5oS5fMSPOiE9KG1MdzlIr_5GG4LkBj3G6598rPi6-oPE5A4eM6ZT3wczyglFmW3RbaPfVxAA9zD0wDv77nnPjnFalr54VJdhDIZMn7jRIPSMtFvPq3dO-y6qHaJtwzVlefp1GUwiaZclb4JyMkIc5EMc1fZsvw87PNotNhNlAQpo64Ht8_35RNYOTmtE0W-xUBn-rtXaHE9BSoyZj3pB3AX4sk6PYSE7eIB3ZQnTMCVvYz9o-KQf6WPmSJfyvvFk8EpDoyHSV6yeVDTyHayUf9KfQk6vfogzGLHTOri9ur_1hlI0GYug"
+
+
 def test_extract(example_token):
     extracted = extract_payload(example_token)
     assert 'profile' in extracted
@@ -40,3 +45,8 @@ def test_bad_extract():
     jwt = format_as_jwt('ill formed string')
     assert len(jwt.split('.')) == 3
     assert extract_payload(jwt) == {}
+
+
+def test_dea_extract(jwt_sample):
+    results = extract_payload(jwt_sample)
+    assert results['DEA'] == 'testDEAvalue'
