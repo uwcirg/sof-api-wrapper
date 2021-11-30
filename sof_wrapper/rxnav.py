2837a88cf9d00b14cdc74af28d76e1389abee15a
Ivan Cvitkovic <ivanc@uw.edu>
2021-11-30 14:13:39 -0800

    Rename variables

--- sof_wrapper/rxnav.py
+++ sof_wrapper/rxnav.py
@@ -29,28 +29,28 @@ def add_drug_classes(med, rxnav_url):
 
     rxnav_response = get_drug_classes(rxcui, rxnav_url)
     drug_class_map = load_drug_class_map()
-    drug_classes = set(
-        drug_class_map[drug_class]
-        for drug_class in drug_class_filter(rxnav_response) if drug_class in drug_class_map
+    cosri_drug_classes = set(
+        drug_class_map[drug_class_name]
+        for drug_class_name in drug_class_filter(rxnav_response) if drug_class_name in drug_class_map
     )
 
-    if not drug_classes:
-
+    if not cosri_drug_classes:
         msg = f"drug class unavailable: {med_text} ({meds})"
         audit_entry(
             msg,
             extra={'tags': ['RxNorm', 'rxnorm-drug-class-not-found']},
-            level='warn')
+            level='warn',
+        )
         # submit error for ELK alerts as this should get attention
         current_app.logger.error(msg)
 
     annotated_med = med.copy()
     med_cc_extensions = annotated_med["medicationCodeableConcept"].get("extension", [])
 
-    for drug_class in drug_classes:
+    for cosri_drug_class in cosri_drug_classes:
         med_cc_extensions.append({
             "url": "http://cosri.org/fhir/drug_class",
-            "valueString": drug_class,
+            "valueString": cosri_drug_class,
         })
 
     annotated_med["medicationCodeableConcept"]["extension"] = med_cc_extensions
