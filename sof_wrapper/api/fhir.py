221ee071cdcd01e994b6b970c959cfe4ab53f612
Ivan Cvitkovic <ivanc@uw.edu>
2025-05-20 11:49:05 -0700

    Passthrough PUT/POST from frontend (#79)
    * Passthrough PUT/POST from frontend
    
    * add missing GET
    
    * Add content-type to CORS allowlist

--- sof_wrapper/api/fhir.py
+++ sof_wrapper/api/fhir.py
@@ -243,8 +243,8 @@ def patient_by_id(id):
 
 
 @blueprint.route('/fhir-router/', defaults={'relative_path': '', 'session_id': None})
-@blueprint.route('/fhir-router/<string:session_id>/<path:relative_path>')
-@blueprint.route('/fhir-router/<string:session_id>/', defaults={'relative_path': ''})
+@blueprint.route('/fhir-router/<string:session_id>/<path:relative_path>', methods=('GET', 'POST', 'PUT'))
+@blueprint.route('/fhir-router/<string:session_id>/', defaults={'relative_path': ''}, methods=('GET', 'POST', 'PUT'))
 def route_fhir(relative_path, session_id):
     g.session_id = session_id
     current_app.logger.debug('received session_id as path parameter: %s', session_id)
@@ -295,6 +295,6 @@ def route_fhir(relative_path, session_id):
 @blueprint.after_request
 def add_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Cache-Control'
+    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Cache-Control, Content-Type'
 
     return response
